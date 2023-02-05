d26738652fad3ab8e23ed64ec7871cac4163a230
Armand <armandbernard1995@gmail.com>
2023-02-05 10:43:32 +0000

    add horizontal padding to the main container

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Experience } from "./Experience";
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="max-w-6xl w-full flex flex-col">
+    <div className="flex justify-center">
+      <div className="max-w-6xl flex flex-col px-8 flex-grow">
         <h1 className="self-center">Armand's Web CV</h1>
         <h2>Summary</h2>
         <Experience />
