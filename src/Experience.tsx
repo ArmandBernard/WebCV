2bc52dbcfc6bf6a1541e03aaa190d5471d7d4899
Armand <armandbernard1995@gmail.com>
2023-02-05 11:47:40 +0000

    Add Redgate experience

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,7 +1,69 @@
+import { TimelineSegment } from "./TimelineSegment";
+
 export const Experience = () => {
   return (
     <>
       <h2>Experience</h2>
+      <TimelineSegment timelineText="Present">
+        <div>
+          <h3 className="font-semibold">
+            Software Engineer at{" "}
+            <a href="https://www.red-gate.com/">Redgate Software</a>
+          </h3>
+          Worked on{" "}
+          <a href="https://www.red-gate.com/products/dba/sql-monitor/">
+            SQL Monitor
+          </a>
+          , a database monitoring tool targeted at database administrators.
+          <ul className="list-disc pl-8 leading-normal">
+            <li>
+              <b>Front-end</b> web-app development using{" "}
+              <b>TypeScript, React and Tailwind</b>.
+            </li>
+            <li>
+              Writing <b>front-end unit tests</b> using Jest
+            </li>
+            <li>
+              <b>MVC</b> and <b>WebAPI</b> back-end development with{" "}
+              <b>.NET Core</b> and C#.
+            </li>
+            <li>
+              Writing <b>back-end unit and integration tests</b> using NUnit,
+              NSubstitute, Fluent Assertions.
+            </li>
+            <li>
+              Writing a wide range of <b>T-SQL</b> scripts for monitoring client
+              dbs and storing data in local SQL Server repository.
+            </li>
+            <li>
+              Improving product <b>accessibility</b>
+            </li>
+            <li>
+              Migrating code from older frameworks such as: jQuery, Knockout.
+            </li>
+          </ul>
+          Heavily involved in the development of internal <b>design systems</b>.
+          <ul className="list-disc pl-8 leading-normal">
+            <li>
+              Choosing the technologies and libraries involved (Yarn, React,
+              Tailwind, MUI).
+            </li>
+            <li>
+              Writing <b>CI/CD</b> pipeline using GitHub Actions, bash, npm.
+            </li>
+            <li>Writing component API guidelines and best practice.</li>
+            <li>
+              Writing guidance on which components to use in what situation.
+            </li>
+            <li>
+              Writing automatic <b>screenshot tests</b>
+            </li>
+            <li>
+              Improving flexibility and <b>accessibility</b>.
+            </li>
+          </ul>
+        </div>
+      </TimelineSegment>
     </>
   );
 };
