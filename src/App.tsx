281072646c38c44c9d550a5f76c0d9331dab14e6
Armand <armandbernard1995@gmail.com>
2023-02-05 11:00:07 +0000

    reduce main container margin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Experience } from "./Experience";
 function App() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-6xl flex flex-col px-8 flex-grow">
+      <div className="max-w-6xl flex flex-col px-4 flex-grow">
         <h1 className="self-center">Armand's Web CV</h1>
         <h2>Summary</h2>
         <Experience />
