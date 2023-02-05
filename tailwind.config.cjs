fd5a07227be37525048f82634b214f1095f763f7
Armand <armandbernard1995@gmail.com>
2023-02-05 08:34:22 +0000

    apply basic dark theming via tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./*.html"],
   theme: {
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
