ef10dbea4de71abc11ef004af100ecb82618f491
Armand <armandbernard1995@gmail.com>
2023-02-05 16:22:55 +0000

    create a tag

---
+++ src/Tag.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent, ReactNode } from "react";
+
+interface TagProps {
+  children: ReactNode;
+}
+
+export const Tag: FunctionComponent<TagProps> = (props) => (
+  <span className="bg-tag-background px-2 rounded-full text-sm">
+    {props.children}
+  </span>
+);
