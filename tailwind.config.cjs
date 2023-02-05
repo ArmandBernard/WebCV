6bf975eefd17c8f18f3331fc5feef69d799dac91
Armand <armandbernard1995@gmail.com>
2023-02-05 16:13:30 +0000

    use parameterised colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./*.html"],
   theme: {
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      ...colors,
+      primary: "var(--primary)",
     },
   },
   plugins: [],
