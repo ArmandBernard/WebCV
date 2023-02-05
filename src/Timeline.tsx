4882206d26a61a6b8effcac9df7f7f3e2754e2da
Armand <armandbernard1995@gmail.com>
2023-02-05 22:19:47 +0000

    wrap classes

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -6,7 +6,10 @@ interface TimeLineProps {
 
 export const Timeline: FunctionComponent<TimeLineProps> = (props) => {
   return (
-    <div className="grid grid-cols-[auto_auto_1fr] py-2 gap-4 items-baseline border-primary border-l-4 ml-2">
+    <div
+      className={`grid grid-cols-[auto_auto_1fr] py-2 gap-4 items-baseline 
+                  border-primary border-l-4 ml-2`}
+    >
       {props.children}
     </div>
   );
