373b6543dcdd6c83cdc101170c61516230f5cdba
Armand <armandbernard1995@gmail.com>
2023-02-05 11:08:31 +0000

    allow multiline timelineText
    via ReactNode and grid changes

--- src/TimelineSegment.tsx
+++ src/TimelineSegment.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, ReactNode } from "react";
 
 interface TimelineSegmentProps {
-  timelineText: string;
+  timelineText: ReactNode;
   children?: ReactNode;
 }
 
@@ -10,11 +10,9 @@ export const TimelineSegment: FunctionComponent<TimelineSegmentProps> = (
 ) => {
   return (
     <div>
-      <div className="grid grid-cols-[auto_1fr] gap-4 items-baseline border-sky-400 border-l-4 ml-2">
-        <div>
-          <span className="inline-block h-4 w-4 ml-[calc(-0.5rem-2px)] mr-4 rounded-full bg-sky-400 align-middle"></span>
-          {props.timelineText}
-        </div>
+      <div className="grid grid-cols-[auto_auto_1fr] gap-4 items-baseline border-sky-400 border-l-4 ml-2">
+        <span className="inline-block h-4 w-4 ml-[calc(-0.5rem-2px)] mt-2 rounded-full bg-sky-400 align-middle" />
+        <div>{props.timelineText}</div>
         <div>{props.children}</div>
       </div>
     </div>
