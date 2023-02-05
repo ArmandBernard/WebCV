219694da4cd482c582d7fe3700c851ad96a41cf1
Armand <armandbernard1995@gmail.com>
2023-02-05 22:20:35 +0000

    use inline syntax

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -4,13 +4,11 @@ interface TimeLineProps {
   children: ReactNode;
 }
 
-export const Timeline: FunctionComponent<TimeLineProps> = (props) => {
-  return (
-    <div
-      className={`grid grid-cols-[auto_auto_1fr] py-2 gap-4 items-baseline 
-                  border-primary border-l-4 ml-2`}
-    >
-      {props.children}
-    </div>
-  );
-};
+export const Timeline: FunctionComponent<TimeLineProps> = (props) => (
+  <div
+    className={`grid grid-cols-[auto_auto_1fr] py-2 gap-4 items-baseline 
+                border-primary border-l-4 ml-2`}
+  >
+    {props.children}
+  </div>
+);
