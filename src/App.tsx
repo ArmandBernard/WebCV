175bd335c3b0378d82ab60f6e74382cf17058afe
Armand <armandbernard1995@gmail.com>
2023-02-05 09:06:16 +0000

    create page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
     <div className="flex flex-col items-center">
       <div className="max-w-6xl w-full flex flex-col">
         <h1 className="self-center">Armand's Web CV</h1>
+        <h2>Summary</h2>
+        <h2>Experience</h2>
+        <h2>Education</h2>
       </div>
     </div>
   );
