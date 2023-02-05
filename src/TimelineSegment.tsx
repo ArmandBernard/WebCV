2f1ffa1b5ae41079f75585b2924b3b56ac2cda7e
Armand <armandbernard1995@gmail.com>
2023-02-05 12:00:21 +0000

    adjust timeline spacing

--- src/TimelineSegment.tsx
+++ src/TimelineSegment.tsx
@@ -9,12 +9,10 @@ export const TimelineSegment: FunctionComponent<TimelineSegmentProps> = (
   props
 ) => {
   return (
-    <div>
-      <div className="grid grid-cols-[auto_auto_1fr] gap-4 items-baseline border-sky-400 border-l-4 ml-2">
-        <span className="inline-block h-4 w-4 ml-[calc(-0.5rem-2px)] mt-2 rounded-full bg-sky-400 align-middle" />
-        <div>{props.timelineText}</div>
-        <div>{props.children}</div>
-      </div>
+    <div className="grid grid-cols-[auto_auto_1fr] py-2 gap-4 items-baseline border-sky-400 border-l-4 ml-2">
+      <span className="inline-block h-4 w-4 ml-[calc(-0.5rem-2px)] mt-1 rounded-full bg-sky-400 align-middle" />
+      <div>{props.timelineText}</div>
+      <div>{props.children}</div>
     </div>
   );
 };
