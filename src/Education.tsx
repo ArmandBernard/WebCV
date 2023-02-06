479af236d8cef711b43ec1426fa72b1c514755a1
Armand <armandbernard1995@gmail.com>
2023-02-06 21:20:47 +0000

    map grades

--- src/Education.tsx
+++ src/Education.tsx
@@ -1,3 +1,5 @@
+import { Tag } from "./Tag";
+
 export const Education = () => (
   <div>
     <h2>Education</h2>
@@ -10,19 +12,24 @@ export const Education = () => (
     </p>
     <h3 className="text-lg my-2">A-levels</h3>
     <ul className="flex justify-between">
-      <li>Maths: A</li>
-      <li>Further Maths: B</li>
-      <li>Physics: B</li>
-      <li>Chemistry: B</li>
+      {["Maths: A", "Further Maths: B", "Physics: B", "Chemistry: B"].map(
+        (grade) => (
+          <li>{grade}</li>
+        )
+      )}
     </ul>
     <h3 className="text-lg my-2">GCSEs</h3>
     <p>10 passes including:</p>
     <ul className="flex justify-between">
-      <li>Maths: A</li>
-      <li>Physics: A</li>
-      <li>Chemistry: A</li>
-      <li>Biology: A</li>
-      <li>English language: B</li>
+      {[
+        "Maths: A",
+        "Physics: A",
+        "Chemistry: A",
+        "Biology: A",
+        "English language: B",
+      ].map((grade) => (
+        <li>{grade}</li>
+      ))}
     </ul>
   </div>
 );
