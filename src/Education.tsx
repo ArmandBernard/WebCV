3b5c1a94b93161c9a5166038ec07c8e60630a7a8
Armand <armandbernard1995@gmail.com>
2023-02-06 21:15:19 +0000

    Add education details

--- src/Education.tsx
+++ src/Education.tsx
@@ -1,5 +1,28 @@
 export const Education = () => (
   <div>
     <h2>Education</h2>
+    <h3 id="university" className="text-lg my-2">
+      University (2013 - 2018)
+    </h3>
+    <p>
+      <b>1st class MEng</b> (Master of Engineering) in Mechanical Engineering
+      from Warwick University.
+    </p>
+    <h3 className="text-lg my-2">A-levels</h3>
+    <ul className="flex justify-between">
+      <li>Maths: A</li>
+      <li>Further Maths: B</li>
+      <li>Physics: B</li>
+      <li>Chemistry: B</li>
+    </ul>
+    <h3 className="text-lg my-2">GCSEs</h3>
+    <p>10 passes including:</p>
+    <ul className="flex justify-between">
+      <li>Maths: A</li>
+      <li>Physics: A</li>
+      <li>Chemistry: A</li>
+      <li>Biology: A</li>
+      <li>English language: B</li>
+    </ul>
   </div>
 );
