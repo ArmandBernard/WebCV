97215cf7966ffbb8cb36e5e4166fc6fb4dd75f17
Armand <armandbernard1995@gmail.com>
2023-02-07 22:43:16 +0000

    add button to toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,58 @@
+import { useCallback, useEffect, useRef } from "react";
 import { AboutMe } from "./AboutMe";
 import "./App.css";
 import { Education } from "./Education";
 import { Experience } from "./Experience";
+import { useLocalStorage } from "./useLocalStorage";
 
 function App() {
+  const htmlRef = useRef(document.documentElement);
+
+  const getTheme = useCallback(
+    () => htmlRef.current.classList.contains("dark"),
+    []
+  );
+
+  const setTheme = useCallback(
+    (dark: boolean) =>
+      dark
+        ? htmlRef.current.classList.add("dark")
+        : htmlRef.current.classList.remove("dark"),
+    []
+  );
+
+  const { get: themePreference, set: setThemePreference } =
+    useLocalStorage("themePreference");
+
+  useEffect(() => {
+    switch (themePreference) {
+      case null:
+        setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+        break;
+      case "light":
+        setTheme(false);
+        break;
+      case "dark":
+      default:
+        setTheme(true);
+    }
+  }, [setTheme, themePreference]);
+
   return (
-    <div className="flex justify-center">
-      <div className="max-w-4xl flex flex-col px-4 flex-grow gap-4 mb-8">
-        <h1 className="self-center m-4">Armand Bernard&apos;s Web CV</h1>
-        <AboutMe />
-        <Experience />
-        <Education />
+    <div>
+      <button
+        className="absolute right-4 top-4"
+        onClick={() => setThemePreference(getTheme() ? "light" : "dark")}
+      >
+        Toggle theme
+      </button>
+      <div className="flex justify-center">
+        <div className="max-w-4xl flex flex-col px-4 flex-grow gap-4 mb-8">
+          <h1 className="self-center m-4">Armand Bernard&apos;s Web CV</h1>
+          <AboutMe />
+          <Experience />
+          <Education />
+        </div>
       </div>
     </div>
   );
