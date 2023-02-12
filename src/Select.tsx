72b8c08d0605da4271cfa405650d603bae9bf97d
Armand <armandbernard1995@gmail.com>
2023-02-12 11:35:39 +0000

    enable proper arrow key navigation

--- src/Select.tsx
+++ src/Select.tsx
@@ -11,9 +11,16 @@ interface SelectProps {
 export const Select: FunctionComponent<SelectProps> = (props) => {
   const [open, setOpen] = useState<boolean>(false);
 
-  const dropdown = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  useOutsideClickHandler(dropdown, () => setOpen(false));
+  const onClose = () => {
+    setOpen(false);
+
+    buttonRef.current?.focus();
+  };
+
+  useOutsideClickHandler(dropdownRef, onClose);
 
   const refs = useMemo(
     () =>
@@ -37,25 +44,52 @@ export const Select: FunctionComponent<SelectProps> = (props) => {
 
   const onClickItem = (option: string) => {
     props.setSelectedOption(option);
-    setOpen(false);
+    onClose();
   };
 
   const onMenuKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Escape") {
-      setOpen(false);
+    switch (e.key) {
+      case "Escape":
+        e.preventDefault();
+
+        onClose();
+        break;
+      case "ArrowDown":
+      case "ArrowRight":
+        {
+          e.preventDefault();
+
+          const focusedIndex = refs.findIndex(
+            (bRef) => bRef.current === document.activeElement
+          );
+          focusItem((focusedIndex + 1) % props.options.length);
+        }
+        break;
+      case "ArrowUp":
+      case "ArrowLeft":
+        {
+          e.preventDefault();
+
+          const focusedIndex = refs.findIndex(
+            (bRef) => bRef.current === document.activeElement
+          );
+          focusItem((focusedIndex - 1) % props.options.length);
+        }
+        break;
     }
   };
 
   return (
     <div className={`flex flex-col ${props.className}`}>
       <button
+        ref={buttonRef}
         className="p-2 border rounded text-left flex justify-between gap-2"
         onClick={onClickExpand}
       >
         {props.selectedOption ?? "Select an option"}
         <span>v</span>
       </button>
-      <div ref={dropdown} className={`relative ${!open && "invisible"}`}>
+      <div ref={dropdownRef} className={`relative ${!open && "invisible"}`}>
         <ul
           className="flex flex-col absolute border rounded w-full"
           onKeyDown={onMenuKeyDown}
@@ -63,6 +97,7 @@ export const Select: FunctionComponent<SelectProps> = (props) => {
           {props.options.map((option, index) => (
             <li key={option}>
               <button
+                tabIndex={-1}
                 ref={refs[index]}
                 className="p-2 w-full text-left whitespace-nowrap"
                 onClick={() => onClickItem(option)}
