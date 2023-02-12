5743c7554586b5789257dc7d23d1c0bb8340d3b1
Armand <armandbernard1995@gmail.com>
2023-02-12 11:13:51 +0000

    use the new dropdown for theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { AboutMe } from "./AboutMe";
 import "./App.css";
 import { Education } from "./Education";
@@ -9,20 +9,13 @@ import { useLocalStorage } from "./useLocalStorage";
 function App() {
   const htmlRef = useRef(document.documentElement);
 
-  const [selectedTheme, setSelectedTheme] = useState<string | null>("auto");
-
-  const getTheme = useCallback(
-    () => htmlRef.current.classList.contains("dark"),
-    []
-  );
-
-  const setTheme = useCallback(
-    (dark: boolean) =>
-      dark
-        ? htmlRef.current.classList.add("dark")
-        : htmlRef.current.classList.remove("dark"),
-    []
-  );
+  const setTheme = useCallback((dark: boolean) => {
+    if (dark) {
+      htmlRef.current.classList.add("dark");
+    } else {
+      htmlRef.current.classList.remove("dark");
+    }
+  }, []);
 
   const { get: themePreference, set: setThemePreference } =
     useLocalStorage("themePreference");
@@ -44,16 +37,11 @@ function App() {
   return (
     <div>
       <Select
-        options={["auto", "dark", "light"]}
-        selectedOption={selectedTheme}
-        setSelectedOption={setSelectedTheme}
+        className="absolute right-4 top-4 w-24"
+        options={["dark", "light"]}
+        selectedOption={themePreference}
+        setSelectedOption={setThemePreference}
       />
-      <button
-        className="absolute right-4 top-4"
-        onClick={() => setThemePreference(getTheme() ? "light" : "dark")}
-      >
-        Toggle theme
-      </button>
       <div className="flex justify-center">
         <div className="max-w-4xl flex flex-col px-4 flex-grow gap-4 mb-8">
           <h1 className="self-center m-4">Armand Bernard&apos;s Web CV</h1>
