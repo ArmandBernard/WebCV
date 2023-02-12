beb316f3642ab79cf8baa6137543fb2520a96da7
Armand <armandbernard1995@gmail.com>
2023-02-12 12:52:37 +0000

    Add a landmark to the main content

--- src/App.tsx
+++ src/App.tsx
@@ -50,12 +50,12 @@ function App() {
       </div>
 
       <div className="flex justify-center">
-        <div className="max-w-4xl flex flex-col px-4 flex-grow gap-4 mb-8">
+        <main className="max-w-4xl flex flex-col px-4 flex-grow gap-4 mb-8">
           <h1 className="self-center m-4">Armand Bernard&apos;s Web CV</h1>
           <AboutMe />
           <Experience />
           <Education />
-        </div>
+        </main>
       </div>
     </div>
   );
