18c4f81c08d4c5df90c48f806318891503b67e06
Armand <armandbernard1995@gmail.com>
2023-02-14 20:35:54 +0000

    Add links header

--- src/Links.tsx
+++ src/Links.tsx
@@ -1,7 +1,10 @@
 export const Links = () => (
-  <div className="flex">
-    <a href="https://www.linkedin.com/in/armand-bernard-8605b8b1/">
-      <img alt="LinkedIn profile" height="40px" src="./LinkedInLogo.svg" />
-    </a>
-  </div>
+  <>
+    <h2>Links</h2>
+    <div className="flex">
+      <a href="https://www.linkedin.com/in/armand-bernard-8605b8b1/">
+        <img alt="LinkedIn profile" height="40px" src="./LinkedInLogo.svg" />
+      </a>
+    </div>
+  </>
 );
