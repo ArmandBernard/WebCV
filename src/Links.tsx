9a8cb875a74dc347eebdbc9aa12e5d2798cca763
Armand <armandbernard1995@gmail.com>
2023-02-14 20:42:19 +0000

    set equal height for logos

--- src/Links.tsx
+++ src/Links.tsx
@@ -3,7 +3,10 @@ export const Links = () => (
     <h2>Links</h2>
     <div className="flex">
       <a href="https://www.linkedin.com/in/armand-bernard-8605b8b1/">
-        <img alt="LinkedIn profile" height="40px" src="./LinkedInLogo.svg" />
+        <img className="h-16" alt="LinkedIn profile" src="./LinkedInLogo.svg" />
+      </a>
+      <a href="https://github.com/ArmandBernard">
+        <img className="h-16" alt="GitHub Profile" src="./github-white.svg" />
       </a>
     </div>
   </>
