0c5d053909c01526c69fee1a3cd783a5b2952dda
Armand <armandbernard1995@gmail.com>
2023-02-14 20:53:43 +0000

    use ThemeContext to render the correct image for the theme

--- src/Links.tsx
+++ src/Links.tsx
@@ -1,13 +1,28 @@
-export const Links = () => (
-  <>
-    <h2>Links</h2>
-    <div className="flex">
-      <a href="https://www.linkedin.com/in/armand-bernard-8605b8b1/">
-        <img className="h-16" alt="LinkedIn profile" src="./LinkedInLogo.svg" />
-      </a>
-      <a href="https://github.com/ArmandBernard">
-        <img className="h-16" alt="GitHub Profile" src="./github-white.svg" />
-      </a>
-    </div>
-  </>
-);
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export const Links = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <>
+      <h2>Links</h2>
+      <div className="flex">
+        <a href="https://www.linkedin.com/in/armand-bernard-8605b8b1/">
+          <img
+            className="h-16"
+            alt="LinkedIn profile"
+            src="./LinkedInLogo.svg"
+          />
+        </a>
+        <a href="https://github.com/ArmandBernard">
+          <img
+            className="h-16"
+            alt="GitHub Profile"
+            src={theme === "dark" ? "./github-white.svg" : "./github.svg"}
+          />
+        </a>
+      </div>
+    </>
+  );
+};
