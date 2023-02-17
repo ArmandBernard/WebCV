c56f55b6998032530c27f711da68ac4ce5294a54
Armand <armandbernard1995@gmail.com>
2023-02-17 18:40:37 +0000

    make sure to get value on mount

--- src/useSystemPreferredTheme.ts
+++ src/useSystemPreferredTheme.ts
@@ -5,9 +5,9 @@ type ThemePreference = "dark" | "light" | null;
 export const useSystemPreferredTheme = () => {
   const [preferredTheme, setPreferredTheme] = useState<ThemePreference>(null);
 
+  // check for both dark, light and unset theme preference
   const handleThemeChange = useCallback(() => {
     let preference: ThemePreference = null;
-
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       preference = "dark";
     } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
@@ -18,11 +18,15 @@ export const useSystemPreferredTheme = () => {
   }, []);
 
   useEffect(() => {
+    // add event listener for theme changes on mount
     const query = window.matchMedia("(prefers-color-scheme: dark)");
-
     query.addEventListener("change", handleThemeChange);
 
+    // call once on mount to get accurate value
+    handleThemeChange();
+
     return () => {
+      // cleanup event listener on unmount
       query.removeEventListener("change", handleThemeChange);
     };
   }, [handleThemeChange]);
