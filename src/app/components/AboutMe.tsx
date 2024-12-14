465b2f49e7e90e25c000e127ab4e1de7bde3985f
Armand <armandbernard1995@gmail.com>
2024-12-14 11:45:36 +0000

    remove inline styles on headings

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -6,7 +6,7 @@ export const AboutMe = () => (
         I am a full-stack software engineer with a passion for design and
         accessibility. I create elegant solutions to problems others ignore.
       </p>
-      <h3 className="text-lg my-2">Experience summary</h3>
+      <h3 className="my-2">Experience summary</h3>
       <p>
         I&apos;ve worked on software for over 5 years, primarily with{" "}
         <b>.NET, TypeScript and React</b>.
@@ -19,7 +19,7 @@ export const AboutMe = () => (
         I have experience planning and running initiatives, small teams /
         projects and doing technical talks.
       </p>
-      <h3 className="text-lg my-2">Skills</h3>
+      <h3 className="my-2">Skills</h3>
       <p>
         I am a <b>fast learner</b>. I can quickly and intuitively grasp
         technical concepts and apply them almost immediately. With a little more
@@ -40,7 +40,7 @@ export const AboutMe = () => (
         effort to make my creations a good experience for people with
         disabilities.
       </p>
-      <h3 className="text-lg my-2">Hobbies</h3>
+      <h3 className="my-2">Hobbies</h3>
       <p>
         I spend my spare time coding, playing video games and building up my
         collection of movies and series.
