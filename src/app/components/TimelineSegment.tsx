366735d497bbe241868b563f9a512c223bc6c8f5
Armand <armandbernard1995@gmail.com>
2024-12-14 13:50:51 +0000

    Vertically stack timeline on small screens

--- src/app/components/TimelineSegment.tsx
+++ src/app/components/TimelineSegment.tsx
@@ -13,7 +13,14 @@ export const TimelineSegment: FunctionComponent<TimelineSegmentProps> = (
       className={`inline-block h-4 w-4 ml-[calc(-0.5rem-2px)] mt-1 rounded-full 
                   bg-primary print-exact`}
     />
-    <div>{props.timelineText}</div>
-    <div>{props.children}</div>
+    <div>
+      {/* On large screens, timeline text shows next to content */}
+      <div className="font-bold max-sm:hidden">{props.timelineText}</div>
+    </div>
+    <div>
+      {/* On small screens, timeline text shows above content */}
+      <div className="font-bold sm:hidden mb-2">{props.timelineText}</div>
+      {props.children}
+    </div>
   </>
 );
