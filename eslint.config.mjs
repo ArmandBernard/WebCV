bcb8432c551317c7feeb15ad7236a4cef971f420
Armand <armandbernard1995@gmail.com>
2025-12-14 14:07:11 +0000

    move to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "eslint/config";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  eslint.configs.recommended,
+  {
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
+    ...react.configs.flat.recommended,
+    ...reactHooks.configs["recommended-latest"],
+    ...jsxA11y.flatConfigs.recommended,
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+
+      quotes: [
+        1,
+        "double",
+        {
+          allowTemplateLiterals: true,
+        },
+      ],
+
+      semi: [1, "always"],
+    },
+  },
+]);
